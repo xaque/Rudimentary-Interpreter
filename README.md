92af8b0a8737687be9582c6f6ed1ee605382034c
xaque <zacka999@gmail.com>
2019-10-14 14:11:00 -0600

    Added variables, lambdas, and if statements

--- README.md
+++ README.md
@@ -10,4 +10,12 @@ The grammer for the language is as follows:
         |       (mod <AE> <AE>)
         |       (collatz <AE>)
         |       (- <AE>)
+		|		<id>
+		|		(if0 <AE> <AE> <AE>)
+		|		with <id> <AE> <AE>)
+		|		(lambda <id> <AE>)
+		|		(<AE> <AE>)
+
+# Only symbols not already in grammar
+<id>	::=		symbol
 ```
--- RudInt.jl
+++ RudInt.jl
@@ -1,5 +1,5 @@
 #
-# Rudimentary Interpreter 0
+# Rudimentary Interpreter 2
 #
 
 module RudInt
@@ -35,6 +35,67 @@ struct BinopNode <: AE
     op::Function
     lhs::AE
     rhs::AE
+end
+
+# <AE> ::= (if0 <AE> <AE> <AE>)
+struct If0Node <: AE
+    cond::AE
+    zerobranch::AE
+    nzerobranch::AE
+end
+
+# <AE> ::= (with <id> <AE> <AE>)
+struct WithNode <: AE
+    sym::Symbol
+    binding_expr::AE
+    body::AE
+end
+
+# <AE> ::= <id>
+struct VarRefNode <: AE
+    sym::Symbol
+end
+
+# <AE> ::= (lambda <id> <AE>)
+struct FuncDefNode <: AE
+    formal::Symbol
+    body::AE
+end
+
+# <AE> ::= (<AE> <AE>)
+struct FuncAppNode <: AE
+    fun_expr::AE
+    arg_expr::AE
+end
+
+
+#             Environment/Return Types
+# ==================================================
+#
+
+abstract type RetVal
+end
+
+abstract type Environment
+end
+
+struct NumVal <: RetVal
+    n::Real
+end
+
+struct ClosureVal <: RetVal
+    formal::Symbol
+    body::AE
+    env::Environment
+end
+
+struct EmptyEnv <: Environment
+end
+
+struct ExtendedEnv <: Environment
+    sym::Symbol
+    val::RetVal
+    parent::Environment
 end
 
 
@@ -85,6 +146,28 @@ function getOpFunction( op::Symbol, dict::Dict )
     return dict[op]
 end
 
+function parse2( expr::Array{Any} )
+    if expr[1] isa Symbol && haskey( unOps, expr[1] )
+        return UnopNode( getOpFunction( expr[1], unOps ), parse( expr[2] ) )
+    end
+    return FuncAppNode( parse(expr[1]), parse(expr[2]) )
+end
+
+function parse3( expr::Array{Any} )
+    if expr[1] == :lambda
+        return FuncDefNode( expr[2], parse(expr[3]) )
+    end
+    return BinopNode( getOpFunction( expr[1], binOps ), parse( expr[2] ), parse( expr[3] ) )
+end
+
+function parse4( expr::Array{Any} )
+    if expr[1] == :if0
+        return If0Node( parse(expr[2]), parse(expr[3]), parse(expr[4]) )
+    elseif expr[1] == :with
+        return WithNode( expr[2], parse(expr[3]), parse(expr[4]) )
+    end
+end
+
 
 #                      Parse
 # ==================================================
@@ -94,21 +177,28 @@ function parse( expr::Number )
     return NumNode( expr )
 end
 
+function parse( expr::Symbol )
+    return VarRefNode( expr )
+end
+
 function parse( expr::Array{Any} )
     len = length(expr)
     if len == 0
         throw( LispError("Empty expression!") )
     elseif len == 2
-        return UnopNode( getOpFunction( expr[1], unOps ), parse( expr[2] ) )
+        return parse2(expr)     
     elseif len == 3
-        return BinopNode( getOpFunction( expr[1], binOps ), parse( expr[2] ), parse( expr[3] ) )
+        return parse3(expr)
+    elseif len == 4
+        return parse4(expr)
     end
     op = expr[1]
     throw( LispError("Invalid expression at: $op") )
 end
 
 function parse( expr::Any )
-  throw( LispError("Invalid type: $expr") )
+    type = typeof(expr)
+    throw( LispError("Invalid type: $type") )
 end
 
 
@@ -116,16 +206,60 @@ end
 # ==================================================
 #
 
-function calc( ast::NumNode )
-    return ast.n
+function calc( ast::NumNode, env::Environment )
+    return NumVal(ast.n)
 end
 
-function calc( ast::BinopNode )
-    return ast.op( calc( ast.lhs ), calc( ast.rhs ) )
+function calc( ast::BinopNode, env::Environment )
+    return NumVal( ast.op( calc( ast.lhs, env ).n, calc( ast.rhs, env ).n ) )
 end
 
-function calc( ast::UnopNode )
-    return ast.op( calc( ast.child ) )
+function calc( ast::UnopNode, env::Environment )
+    return NumVal( ast.op( calc( ast.child, env ).n ) )
+end
+
+function calc( ast::If0Node, env::Environment )
+    cond = calc( ast.cond, env )
+    if cond.n == 0
+        return calc( ast.zerobranch, env )
+    else
+        return calc( ast.nzerobranch, env )
+    end
+end
+
+function calc( ast::WithNode, env::Environment )
+    binding_val = calc( ast.binding_expr, env )
+    ext_env = ExtendedEnv( ast.sym, binding_val, env )
+    return calc( ast.body, ext_env )
+end
+
+function calc( ast::VarRefNode, env::EmptyEnv )
+    throw( Error.LispError("Undefined variable " * string( ast.sym )) )
+end
+
+function calc( ast::VarRefNode, env::ExtendedEnv )
+    if ast.sym == env.sym
+        return env.val
+    else
+        return calc( ast, env.parent )
+    end
+end
+
+function calc( ast::FuncDefNode, env::Environment )
+    return ClosureVal( ast.formal, ast.body , env )
+end
+
+function calc( ast::FuncAppNode, env::Environment )
+    closure_val = calc( ast.fun_expr, env )
+    actual_parameter = calc( ast.arg_expr, env )
+    ext_env = ExtendedEnv( closure_val.formal,
+                           actual_parameter,
+                           closure_val.env )
+    return calc( closure_val.body, ext_env )
+end
+
+function calc( ast::AE )
+    return calc( ast, EmptyEnv() )
 end
 
 
